42ccbf1548b6a1eef6927367411b08c2a4c2d323
Alexander Litvinovich <alexander.litvinovich@silo.ai>
2025-05-12 13:37:44 +0300

    fix

--- install-scripts/immich.sh
+++ install-scripts/immich.sh
@@ -7,17 +7,17 @@
 # Source utility scripts
 source "$UTILS_DIR/print.sh"
 
-local IMMICH_DIR="$HOME/.immich-app/"
+local immich_dir="$HOME/.immich-app/"
 
-if [ ! -d "$IMMICH_DIR" ]; then
-  print_info "Creating Immich directory at $IMMICH_DIR"
-  mkdir -p $IMMICH_DIR
+if [ ! -d "$immich_dir" ]; then
+  print_info "Creating Immich directory at $immich_dir"
+  mkdir -p $immich_dir
 
   print_info "Downloading latest Immich docker-compose.yml"
-  curl -L -o "$IMMICH_DIR/docker-compose.yml" https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
+  curl -L -o "$immich_dir/docker-compose.yml" https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
 
   print_info "Copying example .env file"
-  cp "$ASSETS_DIR/immich.example.env" "$IMMICH_DIR/.env"
+  cp "$ASSETS_DIR/immich.example.env" "$immich_dir/.env"
 
   # Prompt the user for the PostgreSQL password. If no input, use 'postgres' as default
   read -p "Enter your PostgreSQL password (default is 'postgres'): " postgres_password
@@ -27,9 +27,9 @@ if [ ! -d "$IMMICH_DIR" ]; then
   fi
 
   # Update the .env file with the password
-  sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$postgres_password/" "$IMMICH_DIR/.env"
+  sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$postgres_password/" "$immich_dir/.env"
 
   print_success "PostgreSQL password has been set in the .env file."
 else
-  print_info "Immich directory already exists at $IMMICH_DIR"
+  print_info "Immich directory already exists at $immich_dir"
 fi
